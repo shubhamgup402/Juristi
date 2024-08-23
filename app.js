aa8ab65af8d4216decb820021a9706881f007448
Shubham Gupta <97357154+shubhamgup402@users.noreply.github.com>
2024-08-23 11:11:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.post('/generate', async (req, res) => {
 });
 
 // Set port and start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
